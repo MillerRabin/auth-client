9c867b5d414349f5a09d9b2b4f6325b5f98876d0
millerrabin <nemesisv@mail.ru>
2020-03-01 04:07:42 +0300

    added intentionsStorage support

---
+++ intentionsMain.js
@@ -0,0 +1,7 @@
+exports.load = function (intentionStorage) {
+    console.log('loaded raintech auth client');
+};
+
+exports.unload = function (intentionStorage) {
+    console.log('unloaded raintech auth client');
+};
